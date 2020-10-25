42085ed0f04f90de3ae081c276a2a1a41b1b4adc
Dylanshapiro <43762253+Dylanshapiro@users.noreply.github.com>
2020-10-25 01:57:05 -0400

    added LLRBTree

--- Algorithms.py
+++ Algorithms.py
@@ -3,6 +3,16 @@ import sys
 import random
 import numpy as nprand
 import csv
+#############################################################################
+#   SOURCES :                                                               #
+#   https://www.geeksforgeeks.org/left-leaning-red-black-tree-insertion/    # 
+#   https://www.geeksforgeeks.org/heap-sort/                                #
+#   https://www.geeksforgeeks.org/python-program-for-bubble-sort/           #
+#   https://www.geeksforgeeks.org/python-program-for-insertion-sort/        #
+#   https://www.geeksforgeeks.org/python-program-for-selection-sort/        #
+#   https://www.geeksforgeeks.org/python-program-for-merge-sort/            #
+#   https://www.geeksforgeeks.org/heap-sort/                                #
+#############################################################################
 
 class AlgoResult:
     def __init__(self, algoName, arraySize, arrayType, secondsInEpoch):
@@ -33,7 +43,7 @@ def printIfArraySizeHasChanged(origArr, arr):
 def appendResultTolist(arrylength,typeOfArray,algoName, elapsedTime):
     listofresults.append(AlgoResult(algoName,arrylength,typeOfArray,elapsedTime))
 
-def timeThisSortAlgo(sortingAlgorithm, array, typeOfArray, AlgoName):
+def timeThisSortAlgoArray(sortingAlgorithm, array, typeOfArray, AlgoName):
     arr = array.tolist()
     t1 = getTime() 
     error = False
@@ -48,83 +58,55 @@ def timeThisSortAlgo(sortingAlgorithm, array, typeOfArray, AlgoName):
         elapsed = (getTime() - t1)
         printIfArrayIsInOrder(arr)
         printIfArraySizeHasChanged(array.tolist(), arr)
-    appendResultTolist(len(array), typeOfArray, AlgoName, elapsed)
+    appendResultTolist(len(arr), typeOfArray, AlgoName, elapsed)
 
-def timeThisSortAlgoReturnValue(sortingAlgorithm, array, typeOfArray, AlgoName):
+def timeThisSortAlgoRoot(sortingAlgorithm, root, typeOfArray, AlgoName):
     t1 = getTime() 
-    origArray = array.tolist()
     error = False
+    if root == None :
+        return None
+    r = []
     try:
-        r = sortingAlgorithm(origArray)
+        r = sortingAlgorithm(root)
     except Exception as e:
         error = True
         trace = e
     if error:
         elapsed = trace
+        print(trace)
     else:
         elapsed = (getTime() - t1)
         printIfArrayIsInOrder(r)
-        printIfArraySizeHasChanged(origArray, r)
-    appendResultTolist(len(array), typeOfArray, AlgoName, elapsed)
+    appendResultTolist(len(r), typeOfArray, AlgoName, elapsed)
 
 def printArray(arr):
     for i in range(len(arr)): 
         print ("%d" %arr[i]) 
 
-#SOURCE 
-#https://www.geeksforgeeks.org/python-program-for-bubble-sort/
 def bubbleSort(arr): 
     n = len(arr) 
-  
-    # Traverse through all array elements 
-    for i in range(n-1): 
-    # range(n) also work but outer loop will repeat one time more than needed. 
-  
-        # Last i elements are already in place 
+    for i in range(n-1):  
         for j in range(0, n-i-1): 
-  
-            # traverse the array from 0 to n-i-1 
-            # Swap if the element found is greater 
-            # than the next element 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
 
-#SOURCE :
-#https://www.geeksforgeeks.org/python-program-for-insertion-sort/
 def insertionSort(arr): 
-  
-    # Traverse through 1 to len(arr) 
     for i in range(1, len(arr)): 
-  
         key = arr[i] 
-  
-        # Move elements of arr[0..i-1], that are 
-        # greater than key, to one position ahead 
-        # of their current position 
         j = i-1
         while j >=0 and key < arr[j] : 
                 arr[j+1] = arr[j] 
                 j -= 1
         arr[j+1] = key 
 
-#SOURCE 
-#https://www.geeksforgeeks.org/python-program-for-selection-sort/
 def selection_sort(arr):
     for i in range(len(arr)): 
-
-        # Find the minimum element in remaining  
-        # unsorted array 
         min_idx = i 
         for j in range(i+1, len(arr)): 
             if arr[min_idx] > arr[j]: 
-                min_idx = j 
-
-        # Swap the found minimum element with  
-        # the first element         
+                min_idx = j   
         arr[i], arr[min_idx] = arr[min_idx], arr[i] 
 
-#SOURCE 
-#https://www.geeksforgeeks.org/python-program-for-merge-sort/
 def specialMergeSort(arr):
     if (len(arr)<8):
         print("specialMergeSort Algorithm used insertionSort")
@@ -136,16 +118,12 @@ def specialMergeSort(arr):
 
 def mergeSort(arr): 
     if len(arr) >1: 
-        mid = len(arr)//2 # Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
-  
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
-  
+        mid = len(arr)//2 
+        L = arr[:mid]
+        R = arr[mid:]
+        mergeSort(L) 
+        mergeSort(R)  
         i = j = k = 0
-          
-        # Copy data to temp arrays L[] and R[] 
         while i < len(L) and j < len(R): 
             if L[i] < R[j]: 
                 arr[k] = L[i] 
@@ -154,52 +132,33 @@ def mergeSort(arr):
                 arr[k] = R[j] 
                 j+= 1
             k+= 1
-          
-        # Checking if any element was left 
         while i < len(L): 
             arr[k] = L[i] 
             i+= 1
-            k+= 1
-          
+            k+= 1  
         while j < len(R): 
             arr[k] = R[j] 
             j+= 1
             k+= 1
 
-#SOURCE 
-#https://www.geeksforgeeks.org/heap-sort/
 def heapify(arr, n, i): 
-    largest = i # Initialize largest as root 
-    l = 2 * i + 1     # left = 2*i + 1 
-    r = 2 * i + 2     # right = 2*i + 2 
-  
-    # See if left child of root exists and is 
-    # greater than root 
+    largest = i 
+    l = 2 * i + 1
+    r = 2 * i + 2
     if l < n and arr[i] < arr[l]: 
         largest = l 
-  
-    # See if right child of root exists and is 
-    # greater than root 
     if r < n and arr[largest] < arr[r]: 
         largest = r 
-  
-    # Change root, if needed 
     if largest != i: 
-        arr[i],arr[largest] = arr[largest],arr[i] # swap 
-  
-        # Heapify the root. 
+        arr[i],arr[largest] = arr[largest],arr[i]
         heapify(arr, n, largest) 
   
 def heapSort(arr): 
     n = len(arr) 
-  
-    # Build a maxheap. 
     for i in range(n//2 - 1, -1, -1): 
         heapify(arr, n, i) 
-  
-    # One by one extract elements 
     for i in range(n-1, 0, -1): 
-        arr[i], arr[0] = arr[0], arr[i] # swap 
+        arr[i], arr[0] = arr[0], arr[i] 
         heapify(arr, i, 0) 
 
 class Node: 
@@ -224,46 +183,55 @@ def insert(root,node):
                 insert(root.left, node) 
 
 def inorder(root, res):
-    # Recursive travesal 
     if root:
         inorder(root.left,res)
         res.append(root.val)
         inorder(root.right,res)
 
-def treesort(arr):
-    # Build BST
-    if len(arr) == 0:
-        return arr
-    root = Node(arr[0])
-    for i in range(1,len(arr)):
-        insert(root, Node(arr[i]))
-    # Traverse BST in order. 
+def buildBST(arr, ArrayType):
+    newArr = arr.tolist()
+    if len(newArr) == 0:
+        return newArr
+    try:
+        root = Node(newArr[0])
+        for i in range(1,len(newArr)):
+            insert(root, Node(newArr[i]))
+    except Exception as e:
+        appendResultTolist(len(arr), ArrayType, "treesort", "buildBST : " + e.__str__())
+        return None
+    return root
+
+def treesort(root):
     res = []
     inorder(root,res)
     return res
 
-def llrbSort(arr):
-    #TODO: create llrbsort 
+def buildLLRB(arr, ArrayType):
+    node = LLRBTREE()  
+    newArr = arr.tolist()
+    try:
+        for val in newArr :
+            node.root = node.insert(node.root, val)   
+            node.root.color = False 
+    except Exception as e:
+        appendResultTolist(len(arr), ArrayType, "llrbSort", "buildLLRB" + e.__str__())
+        return None
+    return node 
+
+def llrbSort(node):
+    arr = []
+    node.inorder(node.root, arr)
     return arr
 
 def testAllAlgorithms(arr, ArrayType):
- 
-    timeThisSortAlgo(bubbleSort,arr, ArrayType, "bubbleSort") 
-
-    timeThisSortAlgo(insertionSort, arr, ArrayType, "insertionSort") 
-
-    timeThisSortAlgo(selection_sort, arr, ArrayType, "selection_sort")
-
-    timeThisSortAlgo(mergeSort, arr, ArrayType, "mergeSort") 
-
-    timeThisSortAlgo(heapSort, arr, ArrayType, "heapSort") 
-     
-    timeThisSortAlgoReturnValue(treesort,arr, ArrayType, "treesort")
-
-    timeThisSortAlgo(specialMergeSort, arr, ArrayType, "specialMergeSort") 
-
-    timeThisSortAlgo(llrbSort, arr, ArrayType, "llrbSort")
-
+    timeThisSortAlgoArray(bubbleSort,arr, ArrayType, "bubbleSort") 
+    timeThisSortAlgoArray(insertionSort, arr, ArrayType, "insertionSort") 
+    timeThisSortAlgoArray(selection_sort, arr, ArrayType, "selection_sort")
+    timeThisSortAlgoArray(mergeSort, arr, ArrayType, "mergeSort") 
+    timeThisSortAlgoArray(heapSort, arr, ArrayType, "heapSort") 
+    timeThisSortAlgoArray(specialMergeSort, arr, ArrayType, "specialMergeSort") 
+    timeThisSortAlgoRoot(treesort, buildBST(arr,ArrayType), ArrayType, "treesort")
+    timeThisSortAlgoRoot(llrbSort, buildLLRB(arr,ArrayType), ArrayType, "llrbSort")
 
 def createHalfSortedArr(arr):
     return  nprand.partition(arr,(int) (len(arr)/2))
@@ -281,22 +249,83 @@ def createRandomArray(sizeOfArray, numbersToPickfrom=None):
     else:
         numbersToPickfrom =numbersToPickfrom
     sizeOfArray  = sizeOfArray #random.randint(0,1000)
-
     if(numbersToPickfrom < sizeOfArray):
         return nprand.random.choice(sizeOfArray,sizeOfArray,replace=False)
     else:
         return nprand.random.choice(numbersToPickfrom,sizeOfArray,replace=False)
 
-def testAllAlgorithms_(randomArr):
+def testAllAlgorithmsArrayTypes(randomArr):
     testAllAlgorithms(randomArr, "RANDOM")
     testAllAlgorithms(createHalfSortedArr(randomArr), "HALF SORT")
     testAllAlgorithms(nprand.sort(randomArr), "FULL SORT")
 
+class node(object):  
+    def __init__( self ,data):
+        self.data = data  
+        self.left = None  
+        self.right = None  
+        self.color = True  
+  
+class LLRBTREE(object):
+    
+    def __init__(self):
+        self.root = None  
+
+    def rotateLeft(self,myNode): 
+        child = myNode.right 
+        childLeft = child.left  
+        child.left = myNode 
+        myNode.right = childLeft  
+        return child
+    
+    def rotateRight(self,myNode):
+        child = myNode.left 
+        childRight = child.right  
+        child.right = myNode  
+        myNode.left = childRight 
+        return child
+      
+    def isRed(self,myNode):
+        if myNode == None:  
+            return False  
+        return myNode.color == True
+    
+    def swapColors(self, node1,  node2): 
+        temp = node1.color  
+        node1.color = node2.color  
+        node2.color = temp
+
+    def insert(self, myNode,  data):
+        if myNode == None:  
+            return node(data)  
+        if data < myNode.data:  
+            myNode.left = self.insert(myNode.left, data) 
+        elif data > myNode.data:  
+            myNode.right = self.insert(myNode.right, data)  
+        else:
+            return myNode 
+        if self.isRed(myNode.right) and  not self.isRed(myNode.left): 
+            myNode = self.rotateLeft(myNode)
+            self.swapColors(myNode, myNode.left) 
+        if self.isRed(myNode.left) and self.isRed(myNode.left.left):  
+            myNode = self.rotateRight(myNode)
+            self.swapColors(myNode, myNode.right)  
+        if self.isRed(myNode.left) and self.isRed(myNode.right):  
+            myNode.color = not myNode.color
+            myNode.left.color = False  
+            myNode.right.color = False  
+        return myNode   
+    
+    def inorder(self,node,arr): 
+        if not (node == None):   
+            self.inorder(node.left,arr)
+            arr.append(node.data) 
+            self.inorder(node.right,arr)  
+
+
 listofresults =[]
-randomArr = createRandomArray(1000)
-testAllAlgorithms_(randomArr)
-randomArr = createRandomArray(10000)
-testAllAlgorithms_(randomArr)
-randomArr = createRandomArray(50000)
-testAllAlgorithms_(randomArr)
+testAllAlgorithmsArrayTypes(createRandomArray(1000))
+testAllAlgorithmsArrayTypes(createRandomArray(10000))
+testAllAlgorithmsArrayTypes(createRandomArray(100000))
+testAllAlgorithmsArrayTypes(createRandomArray(1000000))
 createCSVWithData(listofresults)
